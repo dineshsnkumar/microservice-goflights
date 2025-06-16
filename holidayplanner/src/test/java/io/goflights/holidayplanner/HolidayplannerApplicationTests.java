ba495ce1a339030600e63944a18db7deafce8bc9
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2025-06-16 15:26:16 +0530

    Inital setup for holiday planner project

---
+++ holidayplanner/src/test/java/io/goflights/holidayplanner/HolidayplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.goflights.holidayplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
